2209097df05cb4affc2cf1df2d9bfc2d933daf54
orianamartina <orianapettinelli04@gmail.com>
2026-07-17 11:11:13 -0300

    fix: points and delete expots

--- UI/result_scene.gd
+++ UI/result_scene.gd
@@ -53,7 +53,7 @@ func _animate_stars() -> void:
 	var max_total := 10000
 	if Global.current_pattern != null:
 		max_total = Global.current_pattern.max_score * 2
-	var star_count := maxi(1, clampi(roundi(float(Global.score) / (float(max_total) * 0.85) * 5.0), 0, 5))
+	var star_count := maxi(1, clampi(floori(float(Global.score) / (float(max_total) * 0.85) * 5.0), 0, 5))
 
 	var star_nodes := stars_container.get_children()
 	for star in star_nodes:
